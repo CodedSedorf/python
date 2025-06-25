4cb2462a35d49ed6be7fdb5075260a714488cc7e
CodedSedorf <sedorfdlc@gmail.com>
2025-06-25 02:26:35 +0100

    function based cal completed

--- cal_func.py
+++ cal_func.py
@@ -22,11 +22,28 @@ def mulMe(num1, num2):
 def powerMe(num1, num2):
   newValue = num1 ** num2
   return newValue
+#
+
 
 firstNum = float(input("Enter first number "))
 secondNum = float(input("Enter second number "))
-operator = input(" Perform basic operation with either +, -, / or *")
+operator = input(" Perform basic operation with either +, -, / or * ")
 
 if operator == "+":
   result = addMe(firstNum, secondNum)
-  print(f"Your final result is {result}")
+  print(f"Your final result is {result} ")
+elif operator == "-":
+  result = subMe(firstNum, secondNum)
+  print(f" Your final result is {result} ")
+elif operator == "*":
+  result = mulMe(firstNum, secondNum)
+  print(f"Your result is {result} ")
+elif operator == "/":
+  if secondNum == 0:
+    print("Sorry, you cannot divide an integer with zero")
+  else:
+    newResult = divMe(firstNum, secondNum)
+    print(f"Your answer is {newResult}")
+elif operator == "**":
+  newResult = powerMe(firstNum, secondNum)
+  print(f"Your final result is {newResult} ")
